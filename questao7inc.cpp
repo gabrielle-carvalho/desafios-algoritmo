0da2c69edafb1294a71531460b5af495dd777bbc
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-07 14:36:49 -0300

    Create questao7inc.cpp

---
+++ questao7inc.cpp
@@ -0,0 +1,21 @@
+//Dizemos que um inteiro positivo n é perfeito se for igual à soma de seus divisores positivos diferentes de n.
+//Exemplo: 6 é perfeito, pois 1+2+3 = 6.
+//Dado um inteiro positivo n, verificar se n é perfeito
+
+#include<stdio.h>
+int n;
+int divisores;
+int soma;
+
+int main(){
+
+printf("Digite um número inteiro positivo: ");
+scanf("%d", &n);
+
+n%2=0
+
+
+
+
+
+}
