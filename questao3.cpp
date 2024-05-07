e6054cd045d9eeddf159f15cdcd8ac070f9a29e8
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-07 14:17:07 -0300

    Create questao3.cpp

---
+++ questao3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor, resultado;
+    resultado = 1;
+    printf("Digite um valor para encontrar seu resultado fatorial: ");
+    scanf("%d", &valor);
+
+    for(int i = 1;i <= valor;){
+        resultado = resultado * i;
+        i++;
+    }
+    printf("\nValor da fatorial de %d: %d", valor, resultado);
+    return 0;
+}
