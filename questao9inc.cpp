51e614d0a82db9e8fea8ccb727a6424a7ce86954
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-08 09:46:45 -0300

    Create questao9inc.cpp

---
+++ questao9inc.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/* leia um conjunto de 
+50 fichas contendo cada uma, o nome, altura e o sexo de 
+uma pessoa. Calcule e imprima :
+a) A maior e a menor altura da turma b) A média de altura das mulheres c) A média de altura da turma
+*/
+int main(){
+    float altura;
+    char nome, sexo;
+    int posicao;
+    posicao=0;
+    
+    printf("Digite o nome da %d", &posicao, "\n");
+    scanf("%d" &nome);
+    
+    printf("Digite o sexo do usuário %c", &sexo, "\n");
+    scanf("%c" &sexo);
+    
+    printf("Digite a altura do usuário %f", &altura, "\n");
+    scanf("%f" &altura);
+
+}
