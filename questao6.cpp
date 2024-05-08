5d89d87986f39702ab3dc5d0ea30466666a04aa7
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-08 11:00:49 -0300

    Create questao6.cpp

---
+++ questao6.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void decimalParaBinario(int decimal) {
+    if (decimal == 0) {
+        printf("0");
+        return;
+    }
+
+    int binario[32]; // Para armazenar o número binário, assumindo um máximo de 32 bits.
+    int i = 0;
+
+    while (decimal > 0) {
+        binario[i] = decimal % 2;
+        decimal = decimal / 2;
+        i++;
+    }
+
+    // Imprimindo o número binário em ordem reversa.
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binario[j]);
+    }
+}
+
+int main() {
+    int numeroDecimal;
+    printf("Digite um número decimal: ");
+    scanf("%d", &numeroDecimal);
+
+    printf("O número binário correspondente é: ");
+    decimalParaBinario(numeroDecimal);
+    printf("\n");
+
+    return 0;
+}
