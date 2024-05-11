b77d88f46b7e0f25c1cc73377dd409a56cf28317
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 17:54:33 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -1,3 +1,5 @@
+//QUESTÃO 4 – Faça um programa que leia um valor numérico. O programa deve ser capaz de somar todos os números anteriores ao valor informado.
+//Ex. valor = 3, deve ser calculado 0+1+2 = 3 Valor = 5, deve ser calculado 0+1+2+3+4 = 10
 #include <stdio.h>
 
 int main()
@@ -7,11 +9,9 @@ int main()
 	int soma;
 	soma=0;
 
-
 	printf("Informe 1 número: ");
 	scanf("%d", &n);
 	cont=n;
-
 
 	while(cont>0){
 		n=n-1;
