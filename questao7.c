80807d580864b8846dc3c5d51a9cd9b6027229b7
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 17:58:42 -0300

    Update questao7.c

--- questao7.c
+++ questao7.c
@@ -1,3 +1,5 @@
+//QUESTÃO 7 - Dizemos que um inteiro positivo n é perfeito se for igual à soma de seus divisores positivos diferentes de n.
+//Exemplo: 6 é perfeito, pois 1+2+3 = 6. Dado um inteiro positivo n, verificar se n é perfeito.
 #include <stdio.h>
 
 int perfeito(int num) {
