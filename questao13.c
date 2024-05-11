b79cb2c717dc3a9c460d3cafcae619238367b506
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 18:04:41 -0300

    Update questao13.c

--- questao13.c
+++ questao13.c
@@ -1,3 +1,4 @@
+//QUESTÃO 13 – Um determinado material radioativo perde metade de sua massa a cada 50 segundos. Dada a massa inicial em gramas, fazer um algoritmo que determine o tempo necessário para que essa massa se torne menor do que 0,5 grama. Escreva a massa inicial, a massa final e o tempo calculado em horas, minutos e segundos.
 #include <stdio.h>
 
 int main() {
