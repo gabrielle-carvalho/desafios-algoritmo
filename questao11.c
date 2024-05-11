cf055674dc7c5c934740b04e6c3234b76d28bd75
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 18:02:59 -0300

    Update questao11.c

--- questao11.c
+++ questao11.c
@@ -1,3 +1,5 @@
+//11 - Dizemos que um número natural n é palíndromo se o 1º algarismo de n é igual ao seu último algarismo, o 2º algarismo de n é igual ao penúltimo algarismo, e assim sucessivamente.
+//Exemplos: o 567765 e 32423 são palíndromos. o 567675 não é palíndromo. Dado um número natural n > 10 , verificar se n é palíndrome. Palíndromos também podem ser identificados em strings. Exemplos: - REVIVER - ANA - ASA - OSSO - RADAR
 #include <stdio.h>
 
 int main() {
