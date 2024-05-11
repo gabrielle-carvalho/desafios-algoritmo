1b5db44fddf335f8a5dad3960999c730840b610e
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 18:00:04 -0300

    Update questao8a.c

--- questao8a.c
+++ questao8a.c
@@ -1,14 +1,12 @@
-/*8 - Calcular o somatório das seguintes séries :
-a) S = 1 + 1/3 + 1/5 + 1/7 + ............+ 1/99
-b) S = 1 – 1/3 + 1/5 – 1/7 + .............para os primeiros 20 termos */
+/*QUESTÃO 8 - Calcular o somatório das seguintes séries :
+a) S = 1 + 1/3 + 1/5 + 1/7 + ............+ 1/99*/
 #include <stdio.h>
 
 int main(){
-    float somatorio = 0;
+    float soma = 0;
 
     for (float impar = 1; impar <= 99; impar+=2){
-        somatorio += (1/impar);
+        soma += (1/impar);
     }
-
-    printf("Resposta: %f\n", somatorio);
+    printf("sOMATÓRIO: %f\n", soma);
 }
