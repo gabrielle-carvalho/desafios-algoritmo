8b48852a251e03759e778924755be90ffabead58
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 17:44:32 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -1,4 +1,4 @@
-//2 - Escrever um algoritmo que lê um conjunto de 4 valores i,a,b,c, onde i é um valor inteiro e positivo e a,b,c são quaisquer valores reais e os escreva. A seguir:
+// QUESTÃO 2 - Escrever um algoritmo que lê um conjunto de 4 valores i,a,b,c, onde i é um valor inteiro e positivo e a,b,c são quaisquer valores reais e os escreva. A seguir:
 //Se i=1 escrever os 3 valores a, b,c em ordem crescente. Se i=2 escrever os 3 valores a,b,c em ordem decrescente. Se i=3 escrever os 3 valores de forma que o maior valor entre a,b,c fica entre os outros
 
 #include <stdio.h>
