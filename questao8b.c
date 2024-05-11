dd5d80f6e1b314ba7c0938bd0386bf9b0b4bcfba
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-05-11 18:01:06 -0300

    Update questao8b.c

--- questao8b.c
+++ questao8b.c
@@ -1,14 +1,16 @@
+//8 - Calcular o somatório das seguintes séries :
+// b) S = 1 – 1/3 + 1/5 – 1/7 + .............para os primeiros 20 termos
 #include <stdio.h>
 
 int main(){
-    float somatorio = 0;
+    float soma = 0;
 
     int sinal = 1;
 
     for (float impar = 1; impar <= 39; impar+=2){
-        somatorio += sinal * (1/impar);
+        soma += sinal * (1/impar);
         sinal *= -1;
     }
 
-    printf("Resposta: %f\n", somatorio);
+    printf("Resposta: %f\n", soma);
 }
